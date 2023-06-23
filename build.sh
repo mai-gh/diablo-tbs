e6174741a94c7783c84ab4b6efdb3af92cdb2f3a
Mai <jea989@gmail.com>
2023-06-22 23:16:31 -0400

    build.sh args as arrays

--- build.sh
+++ build.sh
@@ -2,13 +2,25 @@
 
 test -d ./build || mkdir build
 
-cmake -S ./devilutionX-1.5.0 -B ./build \
-  -DBUILD_TESTING=off \
-  -DNONET=on -DDEBUG=on \
-  -DHAS_KBCTRL=1 \
-  -DKBCTRL_BUTTON_DPAD_LEFT=SDLK_h \
-  -DKBCTRL_BUTTON_DPAD_RIGHT=SDLK_l \
-  -DKBCTRL_BUTTON_DPAD_UP=SDLK_k \
-  -DKBCTRL_BUTTON_DPAD_DOWN=SDLK_j &&
-time ccache cmake --build ./build -j $(getconf _NPROCESSORS_ONLN) &&
+cfg_args=(
+  -S ./devilutionX-1.5.0
+  -B ./build
+  -DBUILD_TESTING=off
+#  -DNONET=on
+#  -DUNPACKED_SAVES=on
+  -DDEBUG=on
+  -DHAS_KBCTRL=1
+  -DKBCTRL_BUTTON_DPAD_LEFT=SDLK_h
+  -DKBCTRL_BUTTON_DPAD_RIGHT=SDLK_l
+  -DKBCTRL_BUTTON_DPAD_UP=SDLK_k
+  -DKBCTRL_BUTTON_DPAD_DOWN=SDLK_j
+)
+
+build_args=(
+  --build ./build
+  -j $(getconf _NPROCESSORS_ONLN)
+)
+
+cmake "${cfg_args[@]}" &&
+time ccache cmake "${build_args[@]}" &&
 ./build/devilutionx
